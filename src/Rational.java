1fadbf9b7009cd0ee7c954ec525ed147cd40db76
engWafula <wafulaallan5@gmail.com>
2022-04-20 18:31:06 +0300

    more addition of reducer fuction

--- src/Rational.java
+++ src/Rational.java
@@ -9,4 +9,56 @@ public class Rational {
         num=n;
         deno=d;
     }
+
+    //default constructor
+
+    public Rational(){
+     num=1;
+     deno=1;
+
+    }
+
+
+    //Accessors
+    public int getNum() {
+        return num;
+    }
+
+    public int getDeno() {
+        return deno;
+    }
+
+    //mutators
+
+
+    public void setNum(int n) {
+        this.num = n;
+    }
+
+    public void setDeno(int d) {
+        this.deno = d;
+    }
+
+    public void reduce(){
+      int g =gcf(num,deno);
+      num=num/g;
+      deno=deno/g;
+    }
+
+    //helper method to give us the greatest common factor of two numbers
+    public int gcf(int x,int y){
+     int result;
+     if(x<y){
+         result =x;
+     }
+  else{
+      result=y;
+     }
+    while(x%result!=0 || y%result!=0)
+        result--;
+     return result;
+    }
+    public String toString() {
+        return  num +"/" + deno;
+    }
 }
--- src/RationalTest.java
+++ src/RationalTest.java
@@ -1,6 +1,14 @@
 public class RationalTest {
 
     public static void main(String[] args){
-        System.out.println("hello word");
+
+        Rational r1 = new Rational(1,3);
+        Rational r2 =new Rational();
+        Rational r3 =new Rational(3,2);
+        Rational r4 =new Rational(10,15);
+
+        r3.setDeno(6);
+        r4.reduce();
+        System.out.println(r4);
     }
 }
