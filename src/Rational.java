84bb9ae446331a784d54b0f112bf7e6c2e0b0b6c
engWafula <wafulaallan5@gmail.com>
2022-04-21 15:16:52 +0300

    new changes

--- src/Rational.java
+++ src/Rational.java
@@ -8,6 +8,7 @@ public class Rational {
     public Rational(int n, int d){
         num=n;
         deno=d;
+        reduce();
     }
 
     //default constructor
@@ -21,6 +22,7 @@ public class Rational {
 
     //Accessors
     public int getNum() {
+
         return num;
     }
 
@@ -32,21 +34,25 @@ public class Rational {
 
 
     public void setNum(int n) {
+
         this.num = n;
+        reduce();
     }
 
-    public void setDeno(int d) {
+    public void setDeno(int d)
+    {
         this.deno = d;
+        reduce();
     }
 
-    public void reduce(){
+    private void reduce(){
       int g =gcf(num,deno);
       num=num/g;
       deno=deno/g;
     }
 
     //helper method to give us the greatest common factor of two numbers
-    public int gcf(int x,int y){
+    private int gcf(int x,int y){
      int result;
      if(x<y){
          result =x;
@@ -57,6 +63,59 @@ public class Rational {
     while(x%result!=0 || y%result!=0)
         result--;
      return result;
+    }
+
+    public  Rational multiply(Rational other){
+        int newNum=this.num*other.num;
+        int newDeno=this.deno* other.deno;
+
+        Rational r =new Rational(newNum,newDeno);
+        return r;
+
+    }
+    public  void  equal(Rational other){
+      if(this.num==other.num&&this.deno==other.deno){
+          System.out.println("the 2 rational numbers are equal");
+      }
+
+      else
+          System.out.println("The 2 rational numbers are not equal");
+
+
+    }
+
+
+    public  Rational divide(Rational other){
+        int newNum=this.num*other.deno;
+        int newDeno=this.deno* other.num;
+
+        Rational r =new Rational(newNum,newDeno);
+        return r;
+
+    }
+
+    public Rational addition(Rational other){
+        int newNum=(this.num*other.deno)+(this.deno*other.num);
+        int newDeno=(this.deno)*(other.deno);
+
+        Rational r =new Rational(newNum,newDeno);
+        return r;
+    }
+
+    public Rational subtraction(Rational other){
+        int newNum=(this.num*other.deno)-(this.deno*other.num);
+        int newDeno=(this.deno)*(other.deno);
+
+        Rational r =new Rational(newNum,newDeno);
+        return r;
+    }
+
+    public Rational division(Rational other){
+        int newNum=(this.num*other.deno)-(this.deno*other.num);
+        int newDeno=(this.deno)*(other.deno);
+
+        Rational r =new Rational(newNum,newDeno);
+        return r;
     }
     public String toString() {
         return  num +"/" + deno;
--- src/RationalTest.java
+++ src/RationalTest.java
@@ -2,13 +2,20 @@ public class RationalTest {
 
     public static void main(String[] args){
 
-        Rational r1 = new Rational(1,3);
+        Rational r1 = new Rational(1,9);
         Rational r2 =new Rational();
-        Rational r3 =new Rational(3,2);
+        Rational r3 =new Rational(1,5);
         Rational r4 =new Rational(10,15);
 
-        r3.setDeno(6);
-        r4.reduce();
-        System.out.println(r4);
+        Rational r5= r3.multiply(r1);
+        Rational r6= r3.addition(r1);
+        Rational r7= r3.subtraction(r1);
+        Rational r8= r3.divide(r4);
+
+
+
+
+
+        System.out.println(r8);
     }
 }
